be1ae4cf79302e563e34bfa20bee0d69bb747083
DEEPAK KUMAR <78249949+kumardeepak85690@users.noreply.github.com>
2024-04-23 12:06:18 +0530

    update in server.js and index.html

--- server/server.js
+++ server/server.js
@@ -2,39 +2,33 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bcryptjs = require("bcryptjs");
-const { fileURLToPath } = require("url");
 const path = require("path");
-
-
-const filename = __filename;
-const dirname = __dirname;
 
 const PORT = 5000;
 const app = express();
-const MONGB_UR = "mongodb+srv://dk705437:job123@cluster0.clmzl6d.mongodb.net/";
+const MONGODB_URI = "mongodb+srv://dk705437:job123@cluster0.clmzl6d.mongodb.net/";
 
-// mdillrwre
+// Enable CORS
 app.use(cors());
-app.use(express.json());
 
-//use the client app
+// Serve static files from the client's dist directory
 app.use(express.static(path.join(__dirname, "/client/dist")));
 
-//render client app
+// Render client app
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/client/dist/index.html"))
 );
 
-// mdillrwre
-app.use(cors());
 app.use(express.json());
-mongoose.connect(MONGB_UR);
+
+// Connect to MongoDB
+mongoose.connect(MONGODB_URI);
 const db = mongoose.connection;
 db.on("error", (err) => {
-  console.error("Mongodb connnection error", err);
+  console.error("MongoDB connection error:", err);
 });
 db.once("open", () => {
-  console.log("Mongodb is connected");
+  console.log("MongoDB is connected");
 });
 
 const userSchema = new mongoose.Schema({
@@ -43,19 +37,23 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 const User = mongoose.model("User", userSchema);
+
 app.post("/register", async (req, res) => {
   try {
-    const hasspassword = bcryptjs.hashSync(req.body.password, 10);
+    const hashedPassword = bcryptjs.hashSync(req.body.password, 10);
     const newUser = new User({
       name: req.body.name,
       email: req.body.email,
-      password: hasspassword,
+      password: hashedPassword,
     });
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
   } catch (error) {
-    console.error("Error druing registration ", error);
-    res.status(500).json({ error: "inter server error" });
+    console.error("Error during registration:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
-app.listen(PORT);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
